822a687487101c053fd969a6bd01cfea0fcb149d
evelinabuharova <evelina.buharova@icloud.com>
2023-12-26 18:51:52 +0300

    add third task

--- src/elseif.java
+++ src/elseif.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class elseif {
+import java.util.Scanner;
+public class elseif {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter number from 1 to 12");
+    int seasons = scanner.nextInt();
+    if (seasons == 12 ||  seasons == 1 || seasons == 2) {
+        System.out.println("Winter");
+    }
+    if (seasons == 3 || seasons == 4 || seasons == 5) {
+        System.out.println("Spring");
+    }
+    if (seasons == 6 || seasons == 7 || seasons == 8) {
+        System.out.println("Summer");
+    }
+    if (seasons == 9 || seasons == 10 || seasons == 11) {
+        System.out.println("Autumn");
+    }
+    }
 }
