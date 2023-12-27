149f2911353c2d7fb483f26fa1c521ee2a19869e
evelinabuharova <evelina.buharova@icloud.com>
2023-12-27 15:10:50 +0300

    add 10 task

---
+++ src/task10.java
@@ -0,0 +1,9 @@
+public class task10 {
+    public static void main(String[] args) {
+        int b = 0;
+        while (b>=-50) {
+            System.out.println(b);
+            b+= (-5) ;
+        }
+    }
+}
