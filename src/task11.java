ed4bd26a4f80563f03e0c7c8140f7e9c4161bc31
evelinabuharova <evelina.buharova@icloud.com>
2023-12-27 15:36:40 +0300

    add 11 task

---
+++ src/task11.java
@@ -0,0 +1,9 @@
+public class task11 {
+    public static void main(String[] args) {
+        int b = 10;
+        while (b<=20) {
+            System.out.println(b*b);
+            b++;
+        }
+    }
+}
