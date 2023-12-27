19b0a3b7ae0dd754fd21ce52f9d4a335981e6db6
evelinabuharova <evelina.buharova@icloud.com>
2023-12-27 15:02:55 +0300

    add 9 task

---
+++ src/task9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class task9 {
+    public static void main(String[] args) {
+        int b = 7;
+        System.out.println(b);
+        int a = 2;
+        while (a<= 14) {
+            int summ= b*a;
+            System.out.println(summ);
+            a++;
+        }
+    }
+}
+
+
